ac38bb41289fd7eebba1f9c6395b791bdca8b452
Josephine <mexy1991@outlook.com>
2019-03-11 10:44:36 +0100

    Added names and student ID's

--- PQHeap.java
+++ PQHeap.java
@@ -1,3 +1,10 @@
+/* * * * * * * * * * * * * * 
+* Forfattere:
+* Josephine Søgaard Andersen, josea18@student.sdu.dk
+* Josias Kure, joulr18@student.sdu.dk 
+* Kasper Jonassen, kajon18@student.sdu.dk 
+* * * * * * * * * * * * * */
+
 public class PQHeap{
 	
 	//Blah
