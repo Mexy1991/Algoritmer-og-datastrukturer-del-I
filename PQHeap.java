2d1eea22d9955b33f4d4824bd974af7cbdd2ba69
Josephine <mexy1991@outlook.com>
2019-03-13 19:06:07 +0100

    Added testfiles

--- PQHeap.java
+++ PQHeap.java
@@ -5,7 +5,22 @@
 * Kasper Jonassen, kajon18@student.sdu.dk 
 * * * * * * * * * * * * * */
 
-public class PQHeap{
-	
-	//Blah
+public class PQHeap implements PQ {
+
+	/*
+	int maxElms;
+
+	public PQHeap(maxElms){
+		PQHeap pqheap = new PQHeap();
+		Elements[] array = new Elements[maxElms];
+	}
+
+	*/
+
+
+	/*
+	public PQHeap(int maxElms){
+		maxElements = maxElms;
+	}
+	*/
 }
